e6329f5437d86a92429b3b42de5a4cf956c50803
winnguyen1905-aladin <winnguyen1905.work@gmail.com>
2025-12-14 15:31:34 +0700

    WIP

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  app.use(helmet());
   // Enable shutdown hooks so onModuleDestroy lifecycle hooks are called
   app.enableShutdownHooks();
 
